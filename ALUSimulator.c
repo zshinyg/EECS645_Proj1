3125e7ff37cd3d17a66c5c7791e37b6d52a503b9
Unknown <noah.riley32@gmail.com>
2018-05-02 20:42:28 -0500

    Finished Enums

--- ALUSimulator.c
+++ ALUSimulator.c
@@ -21,8 +21,16 @@
 #include "RegisterFile_01.h"
 #include "ALUSimulator.h"
 
-enum Function_Code (NOOP_SLL = 0, ADD = 20, ADDU = 21, SUB = 22, SUBU = 23, AND = 24, OR = 25, XOR = 26);
+//Enumerate the function codes for ease of understanding
+enum Function_Code {NOOP_SLL = 0b000000, SRL = 0b000010, SRA = 0b000011,
+ SLLV = 0b000100, SRLV = 0b000110, MFHI = 0b010000, MFLO = 0b010010,
+ MULT = 0b011000, MULTU = 0b011001, DIV = 0b011011, DIVU = 0b011011,
+ ADD = 0b100000, ADDU = 0b100001, SUB = 0b100010, SUBU = 0b100011,
+ AND = 0b100100, OR = 0b100101, XOR = 0b100110, SLT = 0b101010, SLTU = 0b101011};
+
+//Enumerate the OP codes for ease of understanding
 enum Op_Code(Non_I_Type = 0, ADDI = 8, ADDIU = 9, STLI = 10, STLIU = 11);
+
 extern void ALUSimulator( RegisterFile theRegisterFile,
 				uint32_t OpCode,
 				uint32_t Rs, uint32_t Rt, uint32_t Rd,
